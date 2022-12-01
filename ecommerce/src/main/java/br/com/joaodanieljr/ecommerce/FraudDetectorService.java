5067d23d32d5cc9be1f48aef90eef417c021e586
joaodanieljr <joaodanieljr@gmail.com>
2022-11-30 22:11:46 -0300

    Inicio Aula

---
+++ ecommerce/src/main/java/br/com/joaodanieljr/ecommerce/FraudDetectorService.java
@@ -0,0 +1,46 @@
+package br.com.joaodanieljr.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+public class FraudDetectorService {
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties());
+        consumer.subscribe(Collections.singletonList("ECOMMERCE_NEW_ORDER"));
+        while (true) {
+            var records = consumer.poll(Duration.ofMillis(100));
+            if (!records.isEmpty()) {
+                System.out.println("Encontrei " + records.count() );
+                for (var record : records) {
+                    System.out.println("========================================================");
+                    System.out.println("Processing new order");
+                    System.out.println("KEY: " + record.key());
+                    System.out.println("VALUE: " + record.value());
+                    System.out.println("PARTITION: " + record.partition());
+                    System.out.println("OFFSET: " + record.offset());
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+
+        return properties;
+    }
+}
