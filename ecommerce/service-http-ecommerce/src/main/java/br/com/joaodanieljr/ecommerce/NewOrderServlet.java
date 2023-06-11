ed704065fcd950ff5a977d23e0a0f27d21e42f67
joaodanieljr <joaodanieljr@gmail.com>
2023-06-11 20:08:58 -0300

    feature(Finalizando com idempotencia)

--- ecommerce/service-http-ecommerce/src/main/java/br/com/joaodanieljr/ecommerce/NewOrderServlet.java
+++ ecommerce/service-http-ecommerce/src/main/java/br/com/joaodanieljr/ecommerce/NewOrderServlet.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.SQLException;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -18,7 +19,7 @@ public class NewOrderServlet extends HttpServlet {
     private final KafkaDispatcher<String> emailDispatcher = new KafkaDispatcher<>();
 
     @Override
-    public void destroy(){
+    public void destroy() {
         super.destroy();
         orderDispatcher.close();
         emailDispatcher.close();
@@ -40,15 +41,22 @@ public class NewOrderServlet extends HttpServlet {
             var amount = new BigDecimal(req.getParameter("amount"));
             var order = new Order(orderId, amount, email);
 
-            orderDispatcher.send("ECOMMERCE_NEW_ORDER", email,new CorrelationId(NewOrderServlet.class.getSimpleName()), order);
-
-            var emailCode = "Thank you for your order! We are processing your order!";
-            emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email,new CorrelationId(NewOrderServlet.class.getSimpleName()), emailCode);
-            resp.setStatus(HttpServletResponse.SC_OK);
-            resp.getWriter().println("");
+            try (var database = new OrdersDatabase()) {
+                if (database.saveNew(order)) {
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", email, new CorrelationId(NewOrderServlet.class.getSimpleName()), order);
+                    var emailCode = "Thank you for your order! We are processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", email, new CorrelationId(NewOrderServlet.class.getSimpleName()), emailCode);
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.getWriter().println("");
+                } else {
+                    var emailCode = "Old Order!";
+                    resp.setStatus(HttpServletResponse.SC_OK);
+                    resp.getWriter().println("");
+                }
+            }
         } catch (ExecutionException e) {
             throw new ServletException(e);
-        } catch (InterruptedException e) {
+        } catch (InterruptedException | SQLException e) {
             throw new ServletException(e);
         }
     }
